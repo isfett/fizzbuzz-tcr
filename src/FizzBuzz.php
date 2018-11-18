cc99be30b74ac4ecd3501cf9fcac8de51c2453e8
isfett <chris@isfett.com>
2018-11-18 11:47:24 +0100

    wip

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -15,15 +15,16 @@ class FizzBuzz implements FizzBuzzInterface
      */
     public function process(int $input): string
     {
-        if ($input % 3 === 0 && $input % 5 === 0) {
-            return 'FizzBuzz';
+        $output = '';
+
+        if ($input % 3 === 0) {
+            $output .= 'Fizz';
         }
-        elseif ($input % 3 === 0) {
-            return 'Fizz';
+
+        if ($input % 5 === 0) {
+            $output .= 'Buzz';
         }
-        elseif ($input % 5 === 0) {
-            return 'Buzz';
-        }
-        return (string)$input;
+
+        return $output?:(string)$input;
     }
 }
